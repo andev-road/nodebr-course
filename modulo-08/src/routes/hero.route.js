a9def4ee6bbfe1a447de36bd0acd25049eaa5d57
André Treib <andre.schumacher.treib@gmail.com>
2020-05-06 19:45:28 -0300

    end of modulo-10

--- modulo-08/src/routes/hero.route.js
+++ modulo-08/src/routes/hero.route.js
@@ -1,6 +1,11 @@
 const Joi = require('joi');
+const Boom = require('boom');
 
 const BaseRoute = require('./base/base.route');
+
+const failAction = (request, header, error) => {
+    throw error;
+};
 
 class HeroRoutes extends BaseRoute {
     constructor(database) {
@@ -14,9 +19,7 @@ class HeroRoutes extends BaseRoute {
             method: 'GET',
             config: {
                 validate: {
-                    failAction: (request, header, error) => {
-                        throw error;
-                    },
+                    failAction,
                     query: {
                         page: Joi.number().integer().default(1),
                         pageSize: Joi.number().integer().default(20),
@@ -24,19 +27,123 @@ class HeroRoutes extends BaseRoute {
                     }
                 }
             },
-            handler: (request, headers) => {
+            handler: async (request, headers) => {
                 try {
                     const { page, pageSize, name } = request.query;
                     let query = null;
                     if (name) query = {...query, name};
-                    return this.database.read(query, page, pageSize);
+                    return await this.database.read(query, page, pageSize);
                 } catch (err) {
                     console.log("GET /heroes error: ", err);
-                    return "500 Internal server error";
+                    return Boom.internal();
                 }
             }
         }
     }
+
+    create() {
+        return {
+            path: '/heroes',
+            method: 'POST',
+            config: {
+                validate: {
+                    failAction,
+                    payload: {
+                        name: Joi.string().min(2).max(100).required(),
+                        power: Joi.string().max(100).required()
+                    }
+                }
+            },
+            handler: async (request, headers) => {
+                try {
+                    const heroData = request.payload;
+                    return await this.database.create(heroData);
+                } catch (err) {
+                    console.log("POST /heroes error: ", err);
+                    return Boom.internal();
+                }
+            }
+        };
+    }
+
+    updateCompletely() {
+        return {
+            path: '/heroes/{id}',
+            method: 'PUT',
+            config: {
+                validate: {
+                    failAction,
+                    params: {
+                        id: Joi.number().integer().required()
+                    },
+                    payload: {
+                        name: Joi.string().min(2).max(100).required(),
+                        power: Joi.string().max(100).required()
+                    }
+                }
+            },
+            handler: async (request, headers) => {
+                try {
+                    const { id } = request.params;
+                    const heroData = request.payload;
+                    return await this.database.update(id, heroData);
+                } catch (err) {
+                    console.log("PUT /heroes error: ", err);
+                    return Boom.internal();
+                }
+            }
+        };
+    }
+
+    patch() {
+        return {
+            path: '/heroes/{id}',
+            method: 'PATCH',
+            config: {
+                validate: {
+                    failAction,
+                    params: {
+                        id: Joi.number().integer().required()
+                    }
+                }
+            },
+            handler: async (request, headers) => {
+                try {
+                    const { id } = request.params;
+                    const heroData = request.payload;
+                    return await this.database.update(id, heroData);
+                } catch (err) {
+                    console.log("PATCH /heroes error: ", err);
+                    return Boom.internal();
+                }
+            }
+        };
+    }
+
+    delete() {
+        return {
+            path: '/heroes/{id}',
+            method: 'DELETE',
+            config: {
+                validate: {
+                    failAction,
+                    params: {
+                        id: Joi.number().integer().required()
+                    }
+                }
+            },
+            handler: async (request, headers) => {
+                try {
+                    const { id } = request.params;
+                    return await this.database.delete(id);
+                } catch (err) {
+                    console.log("DELETE /heroes error: ", err);
+                    return Boom.internal();
+                }
+            }
+        };
+    }
+
 }
 
 module.exports = HeroRoutes;
